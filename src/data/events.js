bb60f9dbc670bb7c23efe3d998bd9d276774c36f
Guilherme Lima <guilhermelima.dg@gmail.com>
2026-06-01 20:58:52 +0000

    Upando as imagens dos cards

--- src/data/events.js
+++ src/data/events.js
@@ -1,23 +1,28 @@
+// Importando as imagens da pasta assets (subindo um nível com ../)
+import mentirasImg from "../assets/mentiras.png";
+import escapeImg from "../assets/escape.png";
+import mafiaImg from "../assets/mafia.png";
+
 export const eventsData = [
   {
     id: 1,
     title: "JOGO DE MENTIRAS",
     tags: ["FESTA TEMÁTICA", "RPG"],
     description: "Aqui colocamos uma breve descrição da campanha. Ao passar o mouse pela foto é mostrado uma segunda foto e um botão para ver mais.",
-    image: "https://images.unsplash.com/photo-1541094038166-72cb65882e3e?w=500&q=80" 
+    image: mentirasImg // Usando a imagem importada
   },
   {
     id: 2,
     title: "ESCAPE THE BACKROOMS",
     tags: ["GAMEPLAY", "NOITE DO TERROR"],
     description: "Aqui colocamos uma breve descrição da campanha. Ao passar o mouse pela foto é mostrado uma segunda foto e um botão para ver mais.",
-    image: "https://images.unsplash.com/photo-1605806616949-1e87b487cb2a?w=500&q=80"
+    image: escapeImg // Usando a imagem importada
   },
   {
     id: 3,
     title: "NOITE DA MÁFIA",
     tags: ["BOARDGAME", "INVESTIGAÇÃO"],
     description: "Aqui colocamos uma breve descrição da campanha. Ao passar o mouse pela foto é mostrado uma segunda foto e um botão para ver mais.",
-    image: "https://images.unsplash.com/photo-1597950580979-91fb5597fc87?w=500&q=80"
+    image: mafiaImg // Usando a imagem importada
   }
 ];
